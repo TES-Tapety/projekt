8ba221119ed61e417d8f5ba02fb05fb890cb24d6
mreguly <martin.reguly@gmail.com>
2018-03-05 11:50:21 +0100

    uploaded the newest version

---
+++ Tapety/Block.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Drawing;
+
+namespace Minisoft1
+{
+    [Serializable]
+    public class Block
+    {
+        public int id, x, y, startX, startY, width, height, W, H, cell_size;
+        public Color color;
+
+        public Block(int id, int x, int y, int W, int H, int cell_size, Color color)
+        {
+            this.id = id;
+            this.x = x;
+            this.y = y;
+            this.startX = x;
+            this.startY = y;
+            this.color = color;
+            this.W = W;
+            this.H = H;
+            this.cell_size = cell_size;
+            this.width = cell_size * W;
+            this.height = cell_size * H;
+        }
+
+        public void recalculate_shape(int new_size)
+        {
+            this.width = W * new_size;
+            this.height = H * new_size;
+        }
+
+        public void Kresli(Graphics g)
+        {
+            SolidBrush brush = new SolidBrush(this.color);
+            Rectangle rect = new Rectangle(this.x, this.y, this.width, this.height);
+            g.FillRectangle(brush, rect);
+
+            Pen pen = new Pen(Color.Black, 1);
+            g.DrawRectangle(pen, rect);
+        }
+
+        public void Cross(Graphics g, float y)
+        {
+
+        }
+
+        public void KresliEditovaci(Graphics g)
+        {
+            SolidBrush brush = new SolidBrush(this.color);
+            Rectangle rect = new Rectangle(this.x, this.y, this.width, this.height);
+            g.FillRectangle(brush, rect);
+
+            Pen pen = new Pen(Color.Black, 3);
+            g.DrawRectangle(pen, this.x-2, this.y-2, this.width+3, this.height+3);
+            int r = 5;
+            int d = r * 2;
+
+            g.FillEllipse(Brushes.Black, x - d, y - d, d, d);
+            g.FillEllipse(Brushes.Black, x+width, y - d, d, d);
+            g.FillEllipse(Brushes.Black, x - d, y+height, d, d);
+            g.FillEllipse(Brushes.Black, x+width, y+height, d, d);
+        }
+    }
+}
---
+++ Tapety/SaveLoadManager.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Martin
+ * Date: 8.10.2017
+ * Time: 11:44
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Minisoft1
+{
+	/// <summary>
+	/// Description of SettingsManager.
+	/// </summary>
+	public class SaveLoadManager
+	{
+		public SaveLoadManager()
+		{
+		}
+		
+		public Settings load(string fname)
+		// loads settings from file to list
+		{
+			if (File.Exists(fname)) 
+			{ 
+				Settings settings;
+				FileStream s = new FileStream(fname, FileMode.Open);
+				BinaryFormatter f = new BinaryFormatter();
+				settings = f.Deserialize(s) as Settings;
+				s.Close();
+				return settings;
+			}
+			return null;	
+		}
+		
+		public void save(Settings settings, string fname)
+		// saves list of values to file
+		{
+			FileStream s = new FileStream(fname, FileMode.Create);
+			BinaryFormatter f = new BinaryFormatter();
+			f.Serialize(s, settings);
+			s.Close();				
+		}		
+	}
+}
